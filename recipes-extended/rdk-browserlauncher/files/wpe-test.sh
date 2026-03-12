8ec9b9c0891c7157411fac4c160fe5988867a986
BunioFH <buniofh@gmail.com>
2026-03-12 14:27:19 +0100

    RDKEAPPRT-557 scripts cleanup
    Change-Id: If450bcceba0b86a7650765bba7ba9e71b8a0dcd0

--- recipes-extended/rdk-browserlauncher/files/wpe-test.sh
+++ recipes-extended/rdk-browserlauncher/files/wpe-test.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-# export GST_DEBUG="2,webkit*:6"
-# export GST_DEBUG_NO_COLOR="1"
-# export WEBKIT_DEBUG="all"
-# export WEBKIT_INSPECTOR_HTTP_SERVER=0.0.0.0:12345
-
-export WEBKIT_GST_QUIRKS=rialto
-export WEBKIT_GST_HOLE_PUNCH_QUIRK=rialto
-export WEBKIT_GST_ENABLE_AUDIO_MIXER=1
+export WEBKIT_GST_QUIRKS="rialto"
+export WEBKIT_GST_HOLE_PUNCH_QUIRK="rialto"
+export WEBKIT_GST_ENABLE_AUDIO_MIXER="1"
 
 cd /usr/lib/rdk-browserlauncher/
 ./tests/browser_launcher_tests
